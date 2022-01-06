747082d51c8b029c3dc4ce31a2f451f9ff6f6c9e
Menglin Chen <chanmenglin@outlook.com>
2022-01-06 11:50:41 +0800

    Use ts-loader
      On branch master
      Your branch is up to date with 'origin/master'.
    
      Changes to be committed:
    	modified:   build/webpack.base.config.js
    	modified:   package-lock.json
    	modified:   package.json

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -3,6 +3,7 @@
  * webpack 公共环境配置
  */
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
 module.exports = {
     entry: "./src/index.ts",
@@ -17,7 +18,13 @@ module.exports = {
             {
                 test: /\.tsx?$/i,
                 use: [{
-                    loader: "ts-loader"
+                    loader: "ts-loader",
+                    options: {
+                        // 只做类型转换，不做类型检查，可提高构建速度
+                        // 缺点：在编译时不能做类型检查
+                        // 可以借助 fork-ts-checker-webpack-plugin 插件解决类型检查问题
+                        transpileOnly: true, 
+                    }
                 }],
                 exclude: /mode_modules/
             },
@@ -26,6 +33,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/tpl/index.html"
-        })
+        }),
+        new ForkTsCheckerWebpackPlugin()
     ]
 }
